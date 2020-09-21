28b26bce4cba2ac6a0a373635786e3df20d77c3b
habimanasharif <hdidiersharif@gmail.com>
2020-09-21 22:20:44 +0200

    initial commit

---
+++ dist/script.js
@@ -0,0 +1,82 @@
+function write_7(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"7"
+
+}
+function write_8(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"8"
+
+}
+function write_9(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"9"
+
+}
+function write_4(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"4"
+
+}
+function write_5(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"5"
+
+}
+function write_6(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"6"
+
+}
+function write_1(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"1"
+
+}
+function write_2(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"2"
+
+}
+function write_3(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"3"
+
+}
+function write_0(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"0"
+
+}
+function write_x(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"*"
+
+}
+function writedot(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"."
+
+}
+function writep(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"+"
+
+}
+function write_(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"-"
+
+}
+function write_d(){
+    let cur=document.getElementById("text").value
+    document.getElementById("text").value=cur+"/"
+
+}
+function answer(){
+    let cur=document.getElementById("text").value
+
+    document.getElementById("text").value=eval(cur);
+
+}
+
